9c5fb5c1b5a3bb2e8491a05da56b5031addfa248
shinnh2 <nahyeon.shin9@gmail.com>
2023-09-14 03:48:20 +0900

    [refactor/fe-validate] 유효성 검사 기능 리팩토링 - 함수로 분리

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,42 @@ function autobind(
 	};
 	return adjDescriptor;
 }
+//유효성 검사 함수 validater
+interface Validatable {
+	value: string | number;
+	required?: boolean;
+	minLength?: number;
+	maxLength?: number;
+	min?: number;
+	max?: number;
+}
+function validater(validatableInput: Validatable): boolean {
+	let isValid = true;
+	if (validatableInput.required) {
+		isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+	}
+	if (
+		typeof validatableInput.value === "string" &&
+		validatableInput.minLength
+	) {
+		isValid =
+			isValid && validatableInput.value.length >= validatableInput.minLength;
+	}
+	if (
+		typeof validatableInput.value === "string" &&
+		validatableInput.maxLength
+	) {
+		isValid =
+			isValid && validatableInput.value.length <= validatableInput.maxLength;
+	}
+	if (typeof validatableInput.value === "number" && validatableInput.min) {
+		isValid = isValid && validatableInput.value >= validatableInput.min;
+	}
+	if (typeof validatableInput.value === "number" && validatableInput.max) {
+		isValid = isValid && validatableInput.value <= validatableInput.max;
+	}
+	return isValid;
+}
 //사용자의 입력을 받아 개별 프로젝트 신규 등록을 처리하는 클래스
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
@@ -55,24 +91,42 @@ class ProjectInput {
 		event.preventDefault();
 		const userInput = this.gatherUserInput();
 		if (Array.isArray(userInput)) {
-			const [title, desc, people] = userInput;
+			// const [title, desc, people] = userInput;
+			this.clearInputs();
 		}
-		this.clearInputs();
 	}
 	private gatherUserInput(): [string, string, number] | void {
 		const enteredTitle = this.titleInputElement.value;
 		const enteredDescription = this.descriptionInputElement.value;
 		const enteredPeople = this.peopleInputElement.value;
-		if (enteredTitle.trim().length === 0) {
-			alert("제목을 입력해주세요");
+
+		const titleVlidatable: Validatable = {
+			value: enteredTitle,
+			required: true,
+			minLength: 1,
+		};
+		const descVlidatable: Validatable = {
+			value: enteredDescription,
+			required: true,
+			minLength: 5,
+		};
+		const peopleVlidatable: Validatable = {
+			value: +enteredPeople,
+			required: true,
+			min: 1,
+			max: 5,
+		};
+
+		if (!validater(titleVlidatable)) {
+			alert("제목은 1자 이상 입력해야 합니다.");
 			return;
 		}
-		if (enteredDescription.trim().length === 0) {
-			alert("설명을 입력해주세요");
+		if (!validater(descVlidatable)) {
+			alert("설명은 5자 이상 입력해야 합니다.");
 			return;
 		}
-		if (enteredPeople.trim().length === 0) {
-			alert("인원수를 입력해주세요");
+		if (!validater(peopleVlidatable)) {
+			alert("인원수는 1~5로 입력해야 합니다.");
 			return;
 		}
 		return [enteredTitle, enteredDescription, +enteredPeople];
